abfcb93605ca5ba4efb98ce47f891fab802228e3
Georgi Dikov <gvdikov@gmail.com>
2017-06-03 01:55:00 +0200

    implement simple model management and config launcher

--- avb/model_trainer.py
+++ avb/model_trainer.py
@@ -1,31 +1,67 @@
+import os
 import logging
-
+import shutil
+from datetime import datetime
+from numpy import argmin
 from utils.config import load_config
 
-config = load_config('../global_config.yaml')
+config = load_config('global_config.yaml')
 logger = logging.getLogger(__name__)
 
 
 class AVBModelTrainer(object):
-    def __init__(self, model):
+    def __init__(self, model, training_name, overwrite=True):
+        if isinstance(model, str) and model.endswith('.h5'):
+            # interpret the model as path to the stored model and resume training
+            pass
         self.model = model
-
-    def checkpoint_model(self):
-        self.model.save(config['models_dir'])
+        self.overwrite = overwrite
+        self.training_name = training_name
 
     def prepare_training(self):
-        pass
+        if os.path.exists(config['general']['temp_dir']):
+            shutil.rmtree(config['general']['temp_dir'])
+        os.makedirs(config['general']['temp_dir'])
+        self.training_starttime = datetime.now().isoformat()
 
     def finalise_training(self):
-        pass
+        checkpoints = [fname for fname in os.listdir(config['general']['temp_dir']) if 'interrupted' not in fname]
+        best_checkpoint = checkpoints[argmin([float(fname.split('_')[3]) for fname in checkpoints])]
+        if self.overwrite:
+            model_dirname = os.path.join(config['general']['models_dir'], self.training_name)
+        else:
+            model_dirname = os.path.join(config['general']['models_dir'],
+                                         self.training_name + '_{}'.format(datetime.now().isoformat()))
+        if not os.path.exists(model_dirname):
+            os.makedirs(model_dirname)
+        tmp_model_dirname = os.path.join(config['general']['temp_dir'], best_checkpoint)
+        for f in os.listdir(tmp_model_dirname):
+            if f.endswith('.h5'):
+                shutil.move(os.path.join(tmp_model_dirname, f), os.path.join(model_dirname, f))
 
-    def start_training(self, data, batch_size=32, epochs=1, discriminator_repetitions=1):
+        # save some meta info related to the training and experiment:
+        with open(os.path.join(model_dirname, 'meta.txt'), 'w') as f:
+            f.write('Training on {} started on {} and finished on {}'.format(self.training_name,
+                                                                             self.training_starttime,
+                                                                             datetime.now().isoformat()))
+        return model_dirname
+
+    def run_training(self, data, batch_size=32, epochs=1, schedule=None, save_interrupted=False):
         self.prepare_training()
+        model_dirname = os.path.join(config['general']['models_dir'], self.training_name)
         try:
-            self.model.fit(data, batch_size, epochs=epochs, discriminator_repetitions=discriminator_repetitions)
+            schedule = schedule or {'iter_discr': 1, 'iter_encdec': 1}
+            self.model.fit(data, batch_size, epochs=epochs, discriminator_repetitions=schedule['iter_discr'])
+            model_dirname = os.path.join(model_dirname, 'end')
+            self.model.save(model_dirname, deployable_models_only=False, save_metainfo=True)
         except KeyboardInterrupt:
-            self.checkpoint_model()
-            logger.warning("Training has been interrupted and the models "
-                           "have been dumped in {}. Exiting program.".format(1))
+            if save_interrupted:
+                interrupted_dir = os.path.join(model_dirname, 'interrupted_{}'.format(datetime.now().isoformat()))
+                self.model.save(interrupted_dir, deployable_models_only=False, save_metainfo=True)
+                logger.warning("Training has been interrupted and the models "
+                               "have been dumped in {}. Exiting program.".format(interrupted_dir))
+            else:
+                logger.warning("Training has been interrupted.")
         finally:
-            self.finalise_training()
+            model_dirname = self.finalise_training()
+            return model_dirname
--- avb/models/avb.py
+++ avb/models/avb.py
@@ -1,10 +1,8 @@
 import numpy as np
 import os
-from datetime import datetime
-import dill
 from tqdm import tqdm
 
-from keras.models import Model, Input
+from keras.models import Model, Input, model_from_json
 from keras.optimizers import Adam as Optimiser
 from freezable_model import FreezableModel
 
@@ -13,99 +11,134 @@ from .avb_loss import DiscriminatorLossLayer, DecoderLossLayer
 from ..data_iterator import iter_data
 from utils.config import load_config
 
-config = load_config('../global_config.yaml')
-np.random.seed(config['seed'])
+config = load_config('global_config.yaml')
+np.random.seed(config['general']['seed'])
 
 
 class AdversarialVariationalBayes(object):
-    def __init__(self, data_dim, latent_dim=2, noise_dim=None):
+    def __init__(self, data_dim, latent_dim=2, noise_dim=None, restore_models=None, deployable_models_only=False):
 
         self.data_dim = data_dim
         self.noise_dim = noise_dim or data_dim
         self.latent_dim = latent_dim
 
-        encoder = Encoder(data_dim=data_dim, noise_dim=noise_dim, latent_dim=latent_dim)
-        decoder = Decoder(latent_dim=latent_dim, data_dim=data_dim)
-        discriminator = Discriminator(data_dim=data_dim, latent_dim=latent_dim)
+        if restore_models is not None:
+            restorable_models = os.listdir(restore_models)
+            if not deployable_models_only:
+                if 'trainable_encoderdecoder_model.h5' in restorable_models and \
+                                'trainable_discriminator_model.h5' in restorable_models:
+                    with open('trainable_encoderdecoder_model.json', 'r') as j:
+                        self._avb_dec_train = model_from_json(j.read())
+                    self._avb_dec_train.load_weights(os.path.join(restore_models, 'trainable_encoderdecoder_model.h5'))
+                    with open('trainable_discriminator_model.json', 'r') as j:
+                        self._avb_disc_train = model_from_json(j.read())
+                    self._avb_disc_train.load_weights(os.path.join(restore_models, 'trainable_discriminator_model.h5'))
+                else:
+                    ValueError("Could not find `trainable_encoderdecoder_model.h5` "
+                               "and `trainable_discriminator_model.h5` in {}".format(restorable_models))
+            if 'inference_model.h5' in restorable_models and \
+               'generative_model.h5' in restorable_models:
+                    with open('inference_model.json', 'r') as j:
+                        self._inference_model = model_from_json(j.read())
+                    self._inference_model.load_weights(os.path.join(restore_models, 'inference_model.h5'))
+                    with open('generative_model.json', 'r') as j:
+                        self._generative_model = model_from_json(j.read())
+                    self._generative_model.load_weights(os.path.join(restore_models, 'generative_model.h5'))
+            else:
+                ValueError("Could not find `inference_model.h5` "
+                           "and `generative_model.h5` in {}".format(restorable_models))
+        else:
+            encoder = Encoder(data_dim=data_dim, noise_dim=noise_dim, latent_dim=latent_dim)
+            decoder = Decoder(latent_dim=latent_dim, data_dim=data_dim)
+            discriminator = Discriminator(data_dim=data_dim, latent_dim=latent_dim)
 
-        data_input = Input(shape=(data_dim,), name='avb_data_input')
-        noise_input = Input(shape=(noise_dim,), name='avb_noise_input')
-        prior_input = Input(shape=(latent_dim,), name='avb_latent_prior_input')
+            data_input = Input(shape=(data_dim,), name='avb_data_input')
+            noise_input = Input(shape=(noise_dim,), name='avb_noise_input')
+            prior_input = Input(shape=(latent_dim,), name='avb_latent_prior_input')
 
-        posterior_approximation = encoder([data_input, noise_input])
-        reconstruction_log_likelihood = decoder([data_input, posterior_approximation], is_learning=True)
-        discriminator_output_prior = discriminator([data_input, prior_input])
-        discriminator_output_posterior = discriminator([data_input, posterior_approximation])
+            posterior_approximation = encoder([data_input, noise_input])
+            reconstruction_log_likelihood = decoder([data_input, posterior_approximation], is_learning=True)
+            discriminator_output_prior = discriminator([data_input, prior_input])
+            discriminator_output_posterior = discriminator([data_input, posterior_approximation])
 
-        discriminator_loss = DiscriminatorLossLayer(name='disc_loss')([discriminator_output_prior,
-                                                                       discriminator_output_posterior])
-        decoder_loss = DecoderLossLayer(name='dec_loss')([reconstruction_log_likelihood,
-                                                          discriminator_output_posterior])
+            discriminator_loss = DiscriminatorLossLayer(name='disc_loss')([discriminator_output_prior,
+                                                                           discriminator_output_posterior])
+            decoder_loss = DecoderLossLayer(name='dec_loss')([reconstruction_log_likelihood,
+                                                              discriminator_output_posterior])
 
-        self._avb_disc_train = FreezableModel(inputs=[data_input, noise_input, prior_input],
-                                              outputs=discriminator_loss, name_prefix=['disc'])
-        self._avb_dec_train = FreezableModel(inputs=[data_input, noise_input, prior_input],
-                                             outputs=decoder_loss, name_prefix=['dec', 'enc'])
+            # define the trainable models
+            self._avb_disc_train = FreezableModel(inputs=[data_input, noise_input, prior_input],
+                                                  outputs=discriminator_loss, name_prefix=['disc'])
+            self._avb_dec_train = FreezableModel(inputs=[data_input, noise_input],
+                                                 outputs=decoder_loss, name_prefix=['dec', 'enc'])
 
-        self._avb_disc_train.freeze()
-        self._avb_dec_train.unfreeze()
-        self._avb_dec_train.compile(optimizer=Optimiser(lr=1e-3, beta_1=0.5), loss=None, metrics=['accuracy'])
+            self._avb_disc_train.freeze()
+            self._avb_dec_train.unfreeze()
+            self._avb_dec_train.compile(optimizer=Optimiser(lr=1e-3, beta_1=0.5), loss=None)
 
-        self._avb_disc_train.unfreeze()
-        self._avb_dec_train.freeze()
-        self._avb_disc_train.compile(optimizer=Optimiser(lr=1e-3, beta_1=0.5), loss=None)
+            self._avb_disc_train.unfreeze()
+            self._avb_dec_train.freeze()
+            self._avb_disc_train.compile(optimizer=Optimiser(lr=1e-3, beta_1=0.5), loss=None)
 
-        self._inference_model = Model(inputs=[data_input, noise_input], outputs=posterior_approximation)
-        self._generative_model = Model(inputs=prior_input, outputs=decoder(prior_input, is_learning=False))
-        # from keras.utils import plot_model
-        # plot_model(self._avb)
+            # define the deployable model (evaluation only)
+            self._inference_model = Model(inputs=[data_input, noise_input], outputs=posterior_approximation)
+            self._generative_model = Model(inputs=prior_input, outputs=decoder(prior_input, is_learning=False))
 
     def fit(self, data, batch_size=32, epochs=1, discriminator_repetitions=1):
-        data_iterator, iters_per_epoch = iter_data(data, batch_size, mode='training', seed=config['seed'],
+        data_iterator, iters_per_epoch = iter_data(data, batch_size, mode='training', seed=config['general']['seed'],
                                                    latent_dim=self.latent_dim, input_noise_dim=self.noise_dim)
+
+        epoch_loss = np.inf
         for ep in tqdm(xrange(epochs)):
+            epoch_loss_history_encdec = []
+            epoch_loss_history_disc = []
             for it in xrange(iters_per_epoch):
                 data_batch = data_iterator.next()
-                self._avb_dec_train.train_on_batch(data_batch, None)
+                loss_autoencoder = self._avb_dec_train.train_on_batch(data_batch[:-1], None)
+                epoch_loss_history_encdec.append(loss_autoencoder)
                 for _ in xrange(discriminator_repetitions):
-                    self._avb_disc_train.train_on_batch(data_batch, None)
+                    loss_discriminator = self._avb_disc_train.train_on_batch(data_batch, None)
+                    epoch_loss_history_disc.append(loss_discriminator)
+
+            current_epoch_loss = np.mean(epoch_loss_history_encdec) + np.mean(epoch_loss_history_disc)
+            if current_epoch_loss < 0.9 * epoch_loss:
+                epoch_loss = current_epoch_loss
+                self.save(os.path.join(config['general']['temp_dir'], 'ep_{}_loss_{}'.format(ep, epoch_loss)),
+                          deployable_models_only=False)
 
     def infer(self, data, batch_size=32):
-        data_iterator, n_iters = iter_data(data, batch_size, mode='inference', seed=config['seed'],
+        data_iterator, n_iters = iter_data(data, batch_size, mode='inference', seed=config['general']['seed'],
                                            input_noise_dim=self.noise_dim)
         latent_samples = self._inference_model.predict_generator(data_iterator, steps=n_iters)
         return latent_samples
 
     def generate(self, n_samples, batch_size=32):
         n_samples_per_axis = complex(int(np.sqrt(n_samples)))
-        data = np.mgrid[-3:3:n_samples_per_axis, -3:3:n_samples_per_axis].reshape(2, -1).T
+        data = np.mgrid[-300:100:n_samples_per_axis, -50:300:n_samples_per_axis].reshape(2, -1).T
         data_iterator, n_iters = iter_data(data, batch_size=batch_size, mode='generation',
-                                           seed=config['seed'], latent_dim=self.latent_dim)
+                                           seed=config['general']['seed'], latent_dim=self.latent_dim)
         data_probs = self._generative_model.predict_generator(data_iterator, steps=n_iters)
-        sampled_data = np.random.binomial(1, p=data_probs, size=data_probs.shape)
+        sampled_data = np.random.binomial(1, p=data_probs)
         return sampled_data
 
-    def save(self, dirname, deployable_models_only=False):
+    def save(self, dirname, deployable_models_only=False, save_metainfo=False):
         if not os.path.exists(dirname):
             os.makedirs(dirname)
         if not deployable_models_only:
-            timestamp = datetime.now().isoformat()
-            path_to_trainable_models = os.path.join(config['temp_dir'], timestamp)
-            if not os.path.exists(path_to_trainable_models):
-                os.makedirs(path_to_trainable_models)
-            # use cPickle to store all the information necessary to restart the training from that point
-            with open(os.path.join(path_to_trainable_models, 'trainable_discriminator_model'), 'wb') as f:
-                dill.dump(self._avb_disc_train, f)
-            with open(os.path.join(path_to_trainable_models, 'trainable_decoder_model'), 'wb') as f:
-                dill.dump(self._avb_dec_train, f)
-        self._inference_model.save_weights(os.path.join(dirname, 'inference_model'))
-        self._generative_model.save_weights(os.path.join(dirname, 'generative_model'))
+            self._avb_dec_train.save(os.path.join(dirname, 'trainable_encoderdecoder_model.h5'), include_optimizer=True)
+            self._avb_disc_train.save(os.path.join(dirname, 'trainable_discriminator_model.h5'), include_optimizer=True)
+        self._inference_model.save_weights(os.path.join(dirname, 'inference_model.h5'))
+        self._generative_model.save_weights(os.path.join(dirname, 'generative_model.h5'))
 
-    def load(self, model_paths, deployable_models_only=False):
-        self._generative_model.load_weights(model_paths['generative_model'])
-        self._inference_model.load_weights(model_paths['ingerence_model'])
-        if not deployable_models_only:
-            with open(model_paths['trainable_discriminator_model'], 'rb') as f:
-                self._avb_disc_train = dill.load(f)
-            with open(model_paths['trainable_decoder_model'], 'rb') as f:
-                self._avb_dec_train = dill.load(f)
+        if save_metainfo:
+            with open(os.path.join(dirname, 'inference_model.json'), 'w') as f:
+                f.write(self._inference_model.to_json())
+            with open(os.path.join(dirname, 'generative_model.json'), 'w') as f:
+                f.write(self._generative_model.to_json())
+            with open(os.path.join(dirname, 'trainable_encoderdecoder_model.json'), 'w'):
+                f.write(self._avb_dec_train.to_json())
+            with open(os.path.join(dirname, 'trainable_discriminator_model.json'), 'w'):
+                f.write(self._avb_disc_train.to_json())
+            from keras.utils import plot_model
+            plot_model(self._avb_disc_train, to_file='discriminator_model.png')
+            plot_model(self._avb_dec_train, to_file='encoderdecoder_model.png')
